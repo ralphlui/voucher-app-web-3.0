7aa5f36a1677c3f569daad04edbfbddd38efc155
ThazinWai63 <e1332403@u.nus.edu>
2025-04-12 22:04:26 +0800

    Applied to read the access_token from cookies

--- services/user.api.ts
+++ services/user.api.ts
@@ -1,12 +1,37 @@
-import { RootState } from '@/store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+//--v6
+// const userApi = createApi({
+//   reducerPath: 'user',
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: process.env.EXPO_PUBLIC_AUTH_API_URL,
+//     prepareHeaders: (headers) => {
+//       headers.set('Content-Type', 'application/json');
+//       return headers;
+//     },
+//   }),
+//   tagTypes: ['User'],
+//   endpoints: (builder) => ({}),
+// });
 
 const userApi = createApi({
   reducerPath: 'user',
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.EXPO_PUBLIC_AUTH_API_URL,
-    prepareHeaders: (headers) => {
+    baseUrl: process.env.EXPO_PUBLIC_AUTH_API_URL || 'http://localhost:8083/',
+    credentials: 'include',
+    prepareHeaders: async (headers) => {
       headers.set('Content-Type', 'application/json');
+      headers.set('Access-Control-Allow-Credentials', 'true');
+      headers.set('Origin', process.env.EXPO_PUBLIC_AUTH_API_URL || 'http://localhost:8083/');
+
+      // Get and set the token
+      const token = await AsyncStorage.getItem('access_token');
+      if (token) {
+        console.log('Setting token in headers:', token);
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+
       return headers;
     },
   }),
